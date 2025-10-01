42da6990d3b5b0921aa239f4ef932575c0d93f50
Saber <mikumiku654@gmail.com>
2025-10-01 17:42:34 +0300

    xd

---
+++ src/Homework5/Main.java
@@ -0,0 +1,16 @@
+package Homework5;
+
+public class Main {
+    public static void main(String[] args) {
+        String c = "hz";
+        String d = "mz";
+        String sumstrings = sum(c, d);
+        System.out.println(sumstrings);
+    }
+
+    private static String sum(String c, String d) {
+        return (c + "\n" + d);
+    }
+
+
+}
